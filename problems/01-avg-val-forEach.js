e34e0150403c409b82425ff8085c67879373e9af
Benjamin <bengdennis23@gmail.com>
2024-03-30 12:51:48 -0600

    push2

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -13,9 +13,20 @@ console.log(avgVal([])); // null
 
 */
 
-let avgVal = function(arr) {
-    // Your code here 
+let avgVal = function (arr) {
+  if (arr.length === 0) return null; // Return null if array is empty
+
+  let total = 0;
+  arr.forEach(element => {
+    total += element; // Sum up all elements
+  });
+
+  return total / arr.length; // Return the average
 };
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
