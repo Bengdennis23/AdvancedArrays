75805095c469eee32edeea5e5cf017f235102ca6
Benjamin <bengdennis23@gmail.com>
2024-03-30 13:42:09 -0600

    push7

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -12,10 +12,11 @@ console.log(sumWithReduce([])); // 0
 
 */
 
-let sumWithReduce = function(nums) {
-  // Your code here 
+let sumWithReduce = function (nums) {
+  return nums.reduce((adder, val) => adder + val, 0);
 };
-
+console.log(sumWithReduce([10, 3, 5, 2])); // 20
+console.log(sumWithReduce([])); // 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
