306003be1f8091795deda0dbb95ae2eaad7a4a60
Benjamin <bengdennis23@gmail.com>
2024-03-30 13:00:59 -0600

    push5

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -12,10 +12,14 @@ console.log(removeEWords('Enter the building')); // 'building'
 
 */
 
-let removeEWords = function(sentence) {
-    // Your code here 
+let removeEWords = function (sentence) {
+  let words = sentence.split(' ');
+  let filteredWords = words.filter(word => !word.includes('e'));
+  return filteredWords.join(' ');
 };
 
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
