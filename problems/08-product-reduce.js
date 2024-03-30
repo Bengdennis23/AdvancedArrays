e85446e8badd858ea14c651819409f182d53161e
Benjamin <bengdennis23@gmail.com>
2024-03-30 14:07:24 -0600

    push9

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -15,10 +15,13 @@ console.log(productWithReduce([4, 3])); // 12
 
 */
 
-let productWithReduce = function(nums) {
-    // Your code here 
+let productWithReduce = function (nums) {
+  return nums.reduce((multiplier, nums) => multiplier * nums)
 }
 
+
+console.log(productWithReduce([10, 3, 5, 2])); // 300
+console.log(productWithReduce([4, 3])); // 12
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
